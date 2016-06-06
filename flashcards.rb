49d0e907fc4af0c4b0b1fcad17540ecd1d4837d0
Jacob Rapp <jake.rapp322@gmail.com>
2016-06-06 14:01:19 -0700

    Basic database and table creation

--- flashcards.rb
+++ flashcards.rb
@@ -1,4 +1,19 @@
-require "sqlite3"
+# FLASHCARD PROGRAM
+
+require 'sqlite3'
 
 db = SQLite3::Database.new("flashcards.db")
 
+create_table_command = <<-SQL
+	CREATE TABLE IF NOT EXISTS ruby_methods(
+		id INTEGER PRIMARY KEY,
+		method_name varchar(255),
+		function varchar(255),
+		what_classes varchar(255),
+		block_or_argument boolean,
+		output varchar(255),
+		permanent boolean
+	)
+SQL
+
+db.excecute(create_table_command)

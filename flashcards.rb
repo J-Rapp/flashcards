fa495bfa22514cde84378d50f23cbc807370310e
Jacob Rapp <jake.rapp322@gmail.com>
2016-06-06 19:49:40 -0700

    remove class from database columns, add boolean method, unable to get add card method to work

--- flashcards.rb
+++ flashcards.rb
@@ -25,7 +25,6 @@ create_table_command = <<-SQL
 		id INTEGER PRIMARY KEY,
 		name varchar(255),
 		function varchar(255),
-		classes varchar(255),
 		takesarg boolean,
 		output varchar(255),
 		permanent boolean
@@ -37,23 +36,35 @@ db.execute(create_table_command)
 ####### BEGIN METHOD BLOCK #######
 
 # Add card method
-def add_card (db, syntax, purpose, class_use, takes_arg, outputs, perm)
-	db.execute("INSERT INTO ruby_methods (name, function, classes, takesarg, output, permanent) VALUES (?, ?, ?, ?, ?, ?)", [syntax, purpose, class_use, takes_arg, outputs, perm])
+def add_card (database, syntax, purpose, class_use, takes_arg, outputs, perm)
+	database.execute("INSERT INTO ruby_methods (name, function, takesarg, output, permanent) VALUES (?, ?, ?, ?, ?)", [syntax, purpose, takes_arg, outputs, perm])
 end
 
 # Alter card method
-def change_card (db, existing_key, new_value)
-	db.execute("UPDATE ruby_methods SET ? WHERE ?" [new_value, existing_key])
+def change_card (database, existing_key, new_value)
+	database.execute("UPDATE ruby_methods SET ? WHERE ?" [new_value, existing_key])
 end
 
 # Check if duplicate card method
-def duplicate_checker (db, key)
+def duplicate_checker (database, key)
 
 end
 
 # Quiz method
 def card_quiz
 
+end
+
+# Convert to boolean method
+def to_boolean(input)
+	case input[0].downcase
+	when "y"
+		return true
+	when "n"
+		return false
+	else
+		return false
+	end
 end
 
 ####### BEGIN UI #######
@@ -64,31 +75,33 @@ until choice_loop
 	puts "\nWould you like to\n1) Add new flashcards\n2) Alter existing flashcards\n3) Quiz yourself\n4) Exit the program"
 	choice = gets.chomp.to_i
 	case choice 
-	when 1
-		puts "cool"
-		# new_loop = false
-		# until new_loop
-		# 	puts "What is the syntax of the method? (example: .each)"
-		# 	method_syntax = gets.chomp
-		# 	puts "What does the method do?"
-		# 	method_function = gets.chomp
-		# 	puts "What classes can you call this method on?"
-		# 	what_classes = gets.chomp
-		# 	puts "Does this method take and argument or block?"
-		# 	takes_a_or_b = gets.chomp # NEEDS TO BE CONVERTED TO BOOLEAN
-		# 	puts "What does this method put out in the end?"
-		# 	puts_out = gets.chomp
-		# 	puts "Does this method permanently alter the original object?"
-		# 	perm_alter = gets.chomp # NEEDS TO BE CONVERTED TO BOOLEAN
-		# 	add_card(method_syntax, method_function, what_classes, takes_a_or_b, puts_out, perm_alter)
-		# 	puts "Do you want to add another card?"
-		# 	continue = gets.chomp
-		# end
-	when 2
+	when 1 # Add card program
+		new_loop = false
+		until new_loop
+			puts "What is the syntax and applicable class of the method? Example: .each (String)"
+			method_syntax = gets.chomp
+			puts "What does the method do?"
+			method_function = gets.chomp
+			puts "Does this method take and argument or block?"
+			takes_a_or_b = to_boolean(gets.chomp)
+			puts "What does this method put out in the end?"
+			puts_out = gets.chomp
+			puts "Does this method permanently alter the original object?"
+			perm_alter = to_boolean(gets.chomp)
+			p method_syntax, method_function, takes_a_or_b, puts_out, perm_alter
+			# add_card(db, method_syntax, method_function, takes_a_or_b, puts_out, perm_alter)
+			puts "Do you want to add another card?"
+			continue = to_boolean(gets.chomp)
+			if continue == false
+				new_loop = true
+			else
+			end
+		end
+	when 2 # Alter card program
 		puts "ok"
-	when 3
+	when 3 # Quiz program
 		puts "nice"
-	when 4
+	when 4 # End program
 		puts "\nThank you for using the Ruby Method Flashcard Quizzer!"
 		choice_loop = true
 	else
